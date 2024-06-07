b8700f2e5d0b02bd03283f14259be4744aaa8561
Varsha Gupta <vrsa8896@gmail.com>
2024-06-07 15:36:20 +0530

    ..

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () => {
+
+    const[data, setData] = useState([]);
   const AcotorsData = [
     {
       id: "1",
       Name: "Amitabh Bachchan",
+      img:"https://m.media-amazon.com/images/M/MV5BNTk1OTUxMzIzMV5BMl5BanBnXkFtZTcwMzMxMjI0Nw@@._V1_.jpg",
       About:
         "Amitabh Bachchan is an Indian actor who works in Hindi cinema. With a cinematic career spanning over five decades, he has played pivotal roles in over 200 films            ",
     },
@@ -22,29 +25,10 @@ const Profile = () => {
     },
   ];
 
-  const curElement = AcotorsData.map((curElem) => {
-    <>
-      <div className="justify-center flex h-96 pt-10 ">
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNTk1OTUxMzIzMV5BMl5BanBnXkFtZTcwMzMxMjI0Nw@@._V1_.jpg"
-          alt=""
-          className="rounded shadow-xl "
-        />
-      </div>
-      <div className="font-medium px-20 text-center pt-10">
-        <h1 className="font-bold mb-5 text-2xl ">{curElem.Name}</h1>
-        <p>
-          Amitabh Bachchan is an Indian actor who works in Hindi cinema. With a
-          cinematic career spanning over five decades, he has played pivotal
-          roles in over 200 films.
-        </p>
-      </div>
-    </>;
-  });
+ 
   return (
     <div className="h-screen bg-gray-300 sm:ml-56 ">
 
-  
       {/* <div className="justify-center flex h-96 pt-10 ">
         <img
           src="https://m.media-amazon.com/images/M/MV5BNTk1OTUxMzIzMV5BMl5BanBnXkFtZTcwMzMxMjI0Nw@@._V1_.jpg"
@@ -60,6 +44,33 @@ const Profile = () => {
           roles in over 200 films.
         </p>
       </div> */}
+
+
+      {
+
+AcotorsData.map((curElem) => {
+
+   return (
+
+    <>
+    <div className="justify-center flex h-96 pt-10 ">
+        <img
+          src={curElem.img}
+          alt=""
+          className="rounded shadow-xl "
+        />
+      </div>
+      <div className="font-medium px-20 text-center pt-10">
+        <h1 className="font-bold mb-5 text-2xl ">{curElem.Name}</h1>
+        <p>
+        {curElem.About}
+        </p>
+      </div>
+      </>
+   )
+   
+})
+      }
     </div>
   );
 };
